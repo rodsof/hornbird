1fa1416cc13668f3699a59ae376fff6f5d272353
Sofia <sofia9803@hotmail.es>
2020-02-26 18:21:35 -0300

    Procfile

--- index.js
+++ index.js
@@ -2,27 +2,29 @@ const express = require('express');
 const conectarDB = require('./config/db')
 const cors = require('cors');
 
-// create server
-const app = express(); 
+// crear el servidor
+const app = express(); // vamos a estar usando middleware
 
-// conect to the database
+// conectar a la base de datos
 conectarDB();
 
 // habilitar cors
 app.use(cors());
 
-// active express.json to read data entered by users
+// habilitar express.json para leer datos que el usuario coloque
 app.use(express.json({ extended:true }));
 
-// port 
-const port = process.env.port || 4000;
+// puerto de la app
+const port = process.env.PORT || 4000;
 
-// import routes
+// importar rutas
 app.use('/api/usuarios',require('./routes/usuarios')); 
 app.use('/api/auth',require('./routes/auth')); 
 
 
-// start app
+
+
+// arrancar la app
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at port ${port}`);
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
