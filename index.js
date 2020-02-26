ca79d8dd182155e43fb41ae9f7e7729882c30bb7
Sofia <sofia9803@hotmail.es>
2020-02-26 01:46:36 -0300

    deployment

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.use('/api/auth',require('./routes/auth'));
 
 // start app
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at port ${PORT}`);
+    console.log(`Server running at port ${port}`);
 });
