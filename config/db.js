7dad0145c55eb1d57436b3e14689ee93637c8866
Sofia <sofia9803@hotmail.es>
2020-02-27 00:33:17 -0300

    to false uniifeid toplogy

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const conectarDB = async () => {
     try {
         await mongoose.connect(process.env.DB_MONGO, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
+           // useUnifiedTopology: true,
             useFindAndModify: false,
             serverSelectionTimeoutMS: 5000
         })
