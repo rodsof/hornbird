588b860979f4f0d8730163325d0b36a2149619e8
Sofia <sofia9803@hotmail.es>
2020-03-06 22:45:37 -0300

    deployment with model finished

--- controllers/datasetController.js
+++ controllers/datasetController.js
@@ -2,10 +2,9 @@ const Dataset = require('../models/Dataset');
 // obtiene todos los proyectos del usuario actual
 exports.getDataset = async (req, res) => {
     try {
-        const { variable } = req.query;
-        const dataset  = await Dataset.find({},{ variable : 1 }).sort({ date : -1});
-        console.log(dataset);
+        dataset  = await Dataset.find().sort({ date : -1});
         res.json({ dataset });
+        
       } catch (error) {
         console.log(error);
         res.status(500).send("Error");
--- models/Dataset.js
+++ models/Dataset.js
@@ -1,14 +1,105 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const DatasetSchema = mongoose.Schema({
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    energy : {
-        type: String,
-        required:  false
-        }
+  date: {
+    type: String,
+    default: Date.now
+  },
+  chwtin: {
+    type: String,
+    required: false
+  },
+  chwtout: {
+    type: Number,
+    required: false
+  },
+  chwvalveposition: {
+    type: Number,
+    required: false
+  },
+  chwfr: {
+    type: Number,
+    required: false
+  },
+  safanfrequency: {
+    type: Number,
+    required: false
+  },
+  rafanfrequency: {
+    type: Number,
+    required: false
+  },
+  sat: {
+    type: Number,
+    required: false
+  },safr: {
+    type: Number,
+    required: false
+  },
+  saductstpressure: {
+    type: Number,
+    required: false
+  },
+  safanfrequency: {
+    type: Number,
+    required: false
+  },
+  spacet: {
+    type: Number,
+    required: false
+  },
+  ahucop: {
+    type: Number,
+    required: false
+},
+totalpower: {
+  type: Number,
+  required: false
+},
+energymonthly: {
+  type: Number,
+  required: false
+},
+  oat: {
+    type: Number,
+    required: false
+  },
+  spacerh: {
+    type: Number,
+    required: false
+  },
+  oatrh: {
+    type: Number,
+    required: false
+  },
+  vav1fr: {
+    type: Number,
+    required: false
+  },
+  vav2fr: {
+    type: Number,
+    required: false
+  },
+  vav3fr: {
+    type: Number,
+    required: false
+  },
+  vav4fr: {
+    type: Number,
+    required: false
+  },
+  oafr: {
+    type: Number,
+    required: false
+  },
+  oadamperopen: {
+    type: Number,
+    required: false
+  },
+  space: {
+    type: Number,
+    required: false
+  }
 });
 
-module.exports = mongoose.model('Dataset',DatasetSchema);
+module.exports = mongoose.model("Dataset", DatasetSchema);
