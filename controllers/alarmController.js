1754a75a8b2f4886b28dab4964a59e13dced4388
Sofia <sofia9803@hotmail.es>
2020-03-17 14:44:07 -0300

    alarm chagnes

--- controllers/alarmController.js
+++ controllers/alarmController.js
@@ -13,23 +13,27 @@ exports.sendEmail = async (req, res) => {
     });
 
     let mailOptions = {
-        from: "HORNBIRD",
+        from: "info@hornbird.com",
         to: `${req.body.email}`,
         subject: `Hi ${req.body.name}`,
         html: `<b>Email: ${req.body.email}</b><br/> 
         <b>Message:</b><br/>${req.body.message}`
     };
 
+    let id = req.body.id;
+            let name = req.body.name;
+            //, assignDate :  Date.now()
+            let update = { assignTo : name, assignDate :  Date.now() };
+            let filter = {_id : id};
+            let alarm = await Alarm.findOneAndUpdate(filter,{$set: update });
+            let thisAlarm = await Alarm.findById(id); // to check that it was updated i console.log(thisalarm)
+
     transporter.sendMail(mailOptions, (error) => {
         if (error) {
             return res.send(error);
         }
         else{
-            let email = req.body.email;
-            let usuario = Usuario.find({email: email}, { "name": 1});
-            let name = usuario.name;
-            let update = { assignTo : name , assignDate :  Date.now()};
-            let alarm = Alarm.findOneAndUpdate(email,update);
+            
             res.send('Message sent');
         }
     });
