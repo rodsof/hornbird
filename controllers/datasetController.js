410f346cfad9b1775d2c68753b9d10c73b1328bd
sofi <rodriguezsofiaf@gmail.com>
2020-05-09 09:46:59 -0300

    debugging dtset

--- controllers/datasetController.js
+++ controllers/datasetController.js
@@ -2,8 +2,8 @@ const Dataset = require('../models/Dataset');
 // obtiene todos los proyectos del usuario actual
 exports.getDataset = async (req, res) => {
     try {
-        dataset  = await Dataset.find().reverse().limit(15).sort({ date : 1});
-        console.log(dataset);
+        dataset  = await Dataset.find().sort({ date : -1}).limit(15);
+        console.log(dataset.sort({date : 1}));
         res.json({ dataset });
         
       } catch (error) {
