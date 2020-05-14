c046945589c6ee59b6ae7b3e6cd22e9038149945
sofi <rodriguezsofiaf@gmail.com>
2020-05-14 13:35:06 -0300

    testing_ai model

--- models/Testing_ai.js
+++ models/Testing_ai.js
@@ -1,115 +1,114 @@
 const mongoose = require("mongoose");
 
-const Testing_aiSchema = mongoose.Schema({
-
-  "date": {
-      type: Date,
-      default: Date.now
-},
-"chwtin": {
-  type: Number,
-  required: false
-},
-"chwtout": {
-  type: Number,
-  required: false
-},
-"chwvalveposition": {
-  type: Number,
-  required: false
-},
-"chwfr": {
-  type: Number,
-  required: false
-},
-"safanfrequency": {
-  type: Number,
-  required: false
-},
-"rafanfrequency": {
-  type: Number,
-  required: false
-},
-"sat": {
-  type: Number,
-  required: false
-},
-"safr": {
-  type: Number,
-  required: false
-},
-"RA FR, L/s": {
-  type: Number,
-  required: false
-},
-"saductstpressure": {
-  type: Number,
-  required: false
-},
-"vav1fr": {
-  type: Number,
-  required: false
-},
-"vav2fr": {
-  type: Number,
-  required: false
-},
-"vav3fr": {
-  type: Number,
-  required: false
-},
-"vav4fr": {
-  type: Number,
-  required: false
-},
-"oafr":{
-  type: Number,
-  required: false
-},
-"oadamperopen": {
-  type: Number,
-  required: false
-},
-"space": {
-  type: Number,
-  required: false
-},
-"spacerh": {
-  type: Number,
-  required: false
-},
-"spacet": {
-  type: Number,
-  required: false
-},
-"oat": {
-  type: Number,
-  required: false
-},
-"oatrh": {
-  type: Number,
-  required: false
-},
-"ahucop":{
-  type: Number,
-  required: false
-},
-"totalpower": {
-  type: Number,
-  required: false
-},
-"dT Air, C": {
-  type: Number,
-  required: false
-},
-"dT CHW, C": {
-  type: Number,
-  required: false
-},
-"Tin Condenser, C": {
-  type: Number,
-  required: false
-}
+const Testing_ai = mongoose.Schema({
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  chwtin: {
+    type: Number,
+    required: false,
+  },
+  chwtout: {
+    type: Number,
+    required: false,
+  },
+  chwvalveposition: {
+    type: Number,
+    required: false,
+  },
+  chwfr: {
+    type: Number,
+    required: false,
+  },
+  safanfrequency: {
+    type: Number,
+    required: false,
+  },
+  rafanfrequency: {
+    type: Number,
+    required: false,
+  },
+  sat: {
+    type: Number,
+    required: false,
+  },
+  safr: {
+    type: Number,
+    required: false,
+  },
+  "RA FR, L/s": {
+    type: Number,
+    required: false,
+  },
+  saductstpressure: {
+    type: Number,
+    required: false,
+  },
+  vav1fr: {
+    type: Number,
+    required: false,
+  },
+  vav2fr: {
+    type: Number,
+    required: false,
+  },
+  vav3fr: {
+    type: Number,
+    required: false,
+  },
+  vav4fr: {
+    type: Number,
+    required: false,
+  },
+  oafr: {
+    type: Number,
+    required: false,
+  },
+  oadamperopen: {
+    type: Number,
+    required: false,
+  },
+  space: {
+    type: Number,
+    required: false,
+  },
+  spacerh: {
+    type: Number,
+    required: false,
+  },
+  spacet: {
+    type: Number,
+    required: false,
+  },
+  oat: {
+    type: Number,
+    required: false,
+  },
+  oatrh: {
+    type: Number,
+    required: false,
+  },
+  ahucop: {
+    type: Number,
+    required: false,
+  },
+  totalpower: {
+    type: Number,
+    required: false,
+  },
+  "dT Air, C": {
+    type: Number,
+    required: false,
+  },
+  "dT CHW, C": {
+    type: Number,
+    required: false,
+  },
+  "Tin Condenser, C": {
+    type: Number,
+    required: false,
+  },
 });
 
-module.exports = mongoose.model("Testing_ai", Testing_aiSchema);
+module.exports = mongoose.model("Testing_ai", Testing_ai);
