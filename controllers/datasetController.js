e92bca59e1a4dbd7288b710ddd9cb9eb1a27d8a0
sofi <rodriguezsofiaf@gmail.com>
2020-05-14 13:07:11 -0300

    delete energymonthly field

--- controllers/datasetController.js
+++ controllers/datasetController.js
@@ -6,6 +6,7 @@ exports.getDataset = async (req, res) => {
     try {
        // dataset  = await Dataset.find().sort({ date : -1}).limit(15);
        dataset  = await Testing_ai.find().sort({ date : -1}).limit(15);
+       console.log(dataset);
         res.json({ dataset });
       } catch (error) {
         console.log(error);
